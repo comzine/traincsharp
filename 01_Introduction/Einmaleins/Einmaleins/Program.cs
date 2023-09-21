2ae0867021fd313ae4d4b95c44a6026ae058e702
Tobias Weber <comzine@gmail.com>
2023-09-21 12:56:03 +0200

    Einmaleins and InterestCalc added

---
+++ 01_Introduction/Einmaleins/Einmaleins/Program.cs
@@ -0,0 +1,8 @@
+﻿for (int j = 1; j <= 10; j++)
+{
+    for (int i = j; i <= j * 10; i += j)
+    {
+        Console.Write("{0,4}", i);
+    }
+    Console.WriteLine();
+}
---
+++ 01_Introduction/InterestCalc/InterestCalc/Program.cs
@@ -0,0 +1,45 @@
+﻿double kapital, zinsen, zinssatz, kapitalneu;
+int jahr, zuBerechnendeJahre;
+string eingabe;
+
+Console.WriteLine("Kapitalberechnung");
+Console.WriteLine("Mit diesem Programm " +
+    "werden Zinsen " +
+    "sowie das verzinste Kapital berechnet.");
+
+int w = Console.WindowWidth;
+
+for (int i = 0; i < w; i++)
+{
+    Console.Write("-");
+}
+Console.WriteLine();
+
+Console.Write("{0,20}", "Startkapital: ");
+eingabe = Console.ReadLine();
+kapital = Convert.ToDouble(eingabe);
+Console.Write("{0,20}", "Zinssatz: ");
+eingabe = Console.ReadLine();
+zinssatz = Convert.ToDouble(eingabe);
+Console.Write("{0,20}", "Jahre: ");
+eingabe = Console.ReadLine();
+zuBerechnendeJahre = Convert.ToInt32(eingabe);
+
+Console.WriteLine("{0,4}\t{1,8}\t{2,8}",
+    "Jahr", "Zinsen", "Kapital");
+
+zinsen = kapital / 100 * zinssatz;
+kapitalneu = kapital + zinsen;
+jahr = 1;
+
+Console.WriteLine("{0,4}\t{1,8:F2}\t{2,8:F2}",
+    jahr, zinsen, kapitalneu);
+for (jahr = 2; jahr <= zuBerechnendeJahre; jahr++)
+{
+    kapital = kapitalneu;
+    zinsen = kapital / 100 * zinssatz;
+    kapitalneu = kapital + zinsen;
+
+    Console.WriteLine("{0,4}\t{1,8:F2}\t{2,8:F2}",
+        jahr, zinsen, kapitalneu);
+}
