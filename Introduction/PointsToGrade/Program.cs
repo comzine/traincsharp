a940f4b116c4e6cd67f4a80f00a94fa436e2b4c9
Tobias Weber <comzine@gmail.com>
2023-11-15 08:43:47 +0100

    Added PointsToGrade missing README

---
+++ Introduction/PointsToGrade/Program.cs
@@ -0,0 +1,20 @@
+﻿double maxPoints, points = 0;
+double grade;
+
+do {
+    Console.Write("Enter the maximum points: ");
+} while (!double.TryParse(Console.ReadLine(), out maxPoints));
+
+while (points >= 0)
+{
+    do {
+        Console.Write("Enter the points: ");
+    } while (!double.TryParse(Console.ReadLine(), out points));
+
+    if (points < 0)
+        break;
+
+    grade = 6.0 - 5.0 * points / maxPoints;
+
+    Console.WriteLine("Grade: {0:F1}", grade);
+}
