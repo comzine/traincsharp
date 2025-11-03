39a2c549c9adbf2c0b9dcae3a0f418d76fb758f7
Tobias Weber <comzine@gmail.com>
2025-11-03 11:21:52 +0100

    feat: Simplify Leap Year checker by removing loop and direct input handling

--- Beginners/LeapYear/Program.cs
+++ Beginners/LeapYear/Program.cs
@@ -6,47 +6,38 @@ class Program
     {
         Console.WriteLine("Willkommen zum Schaltjahr-Prüfer!");
 
-        while (true)
+        Console.Write("Bitte geben Sie ein Jahr ein: ");
+        string input = Console.ReadLine();
+
+
+        int jahr = Convert.ToInt32(input);
+
+        bool istSchaltjahr;
+
+        if (jahr % 400 == 0)
         {
-            Console.Write("Bitte geben Sie ein Jahr ein (oder 'q' zum Beenden): ");
-            string input = Console.ReadLine();
+            istSchaltjahr = true;
+        }
+        else if (jahr % 100 == 0)
+        {
+            istSchaltjahr = false;
+        }
+        else if (jahr % 4 == 0)
+        {
+            istSchaltjahr = true;
+        }
+        else
+        {
+            istSchaltjahr = false;
+        }
 
-            if (input.ToLower() == "q")
-            {
-                break;
-            }
-
-            int year = Convert.ToInt32(input);
-
-            bool istSchaltjahr;
-
-            if (jahr % 400 == 0)
-            {
-                istSchaltjahr = true;
-            }
-            else if (jahr % 100 == 0)
-            {
-                istSchaltjahr = false;
-            }
-            else if (jahr % 4 == 0)
-            {
-                istSchaltjahr = true;
-            }
-            else
-            {
-                istSchaltjahr = false;
-            }
-
-            if (istSchaltjahr)
-            {
-                Console.WriteLine($"{year} ist ein Schaltjahr.");
-            }
-            else
-            {
-                Console.WriteLine($"{year} ist kein Schaltjahr.");
-            }
-
-            Console.WriteLine();
+        if (istSchaltjahr)
+        {
+            Console.WriteLine($"{jahr} ist ein Schaltjahr.");
+        }
+        else
+        {
+            Console.WriteLine($"{jahr} ist kein Schaltjahr.");
         }
 
         Console.WriteLine("Vielen Dank für die Verwendung des Schaltjahr-Prüfers!");
